3756a41128b93b169bee15532de0a07207dde74a
小針寛乃 <kobarihirono@kobarihiroshinonoMacBook-Pro.local>
2024-04-06 18:15:40 +0900

    [new]基礎学習

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      }
+    },
   },
   plugins: [],
 };
